6d19ae2d764709f2c726f50a771aa4d575785ae1
TechPodx <lahirurasi@gmail.com>
2024-02-27 10:12:12 +0000

    adding rest of the code for transaction tracker

---
+++ Simple Expense Tracker/expence-tracker/README.md
@@ -0,0 +1,11 @@
+# Simple Expence Tracker
+
+The app allows you to set budgets for various categories, such as food and transportation, and track transactions in those categories. It then sums your spending in each category to calculate the amount of money that remains to be spent.
+
+## Preview
+
+![preview]()
+
+## How It Works
+
+The Expense Tracker supports nine expense categories. Users can set their expected/budgeted expenses for each category. When they incur actual expenses in any category, they can update it in the "New Transaction" section. This action will reduce the remaining funds in the tracker and also display the new transaction under the "Transaction" section.
--- Simple Expense Tracker/expence-tracker/src/features/budgets/budgetsSlice.js
+++ Simple Expense Tracker/expence-tracker/src/features/budgets/budgetsSlice.js
@@ -1,3 +1,5 @@
+import { createSlice } from "@reduxjs/toolkit";
+
 export const CATEGORIES = [
   "housing",
   "food",
@@ -14,7 +16,26 @@ const initialState = CATEGORIES.map((category) => ({
   amount: 0,
 }));
 
-export const editBudget = (budget) => {
+export const budgetsSlice = createSlice({
+  name: "budgets",
+  initialState: initialState,
+  reducers: {
+    editBudget: (state, action) => {
+      const { category, amount } = action.payload;
+      const budgetToUpdate = state.find(
+        (budget) => budget.category === category
+      );
+      if (budgetToUpdate) {
+        budgetToUpdate.amount = amount;
+      }
+    },
+  },
+});
+
+// Since I have used createSlice, I am gonna commented out below
+// stand-alone action creator and reducer as it is providing the same functionality
+
+/*export const editBudget = (budget) => {
   return {
     type: "budgets/editBudget",
     payload: budget,
@@ -34,7 +55,9 @@ const budgetsReducer = (state = initialState, action) => {
     default:
       return state;
   }
-};
+};*/
 
 export const selectBudgets = (state) => state.budgets;
-export default budgetsReducer;
+
+export const { editBudget } = budgetsSlice.actions;
+export default budgetsSlice.reducer;
